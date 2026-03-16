62e89e2be5959d28b352b523a3a5fd4d235957e9
Mike <michel200212345+github@gmail.com>
2026-03-16 15:08:09 +0300

    Added mlp regressor

---
+++ models/nn_regressor.py
@@ -0,0 +1,21 @@
+from sklearn.neural_network import MLPRegressor
+
+class MLPRegressionModel:
+
+    def __init__(self, **kwargs):
+        default_params = {
+            "hidden_layer_sizes": (64, 32),
+            "activation": "relu",
+            "solver": "adam",
+            "max_iter": 300,
+            "random_state": 42,
+        }
+        default_params.update(kwargs)
+        self.model = MLPRegressor(**default_params)
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+        return self
+
+    def predict(self, X):
+        return self.model.predict(X)
